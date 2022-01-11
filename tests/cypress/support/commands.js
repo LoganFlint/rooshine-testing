085dffc0110e1c4fff1d6c59490ea52d1585d930
Danielle Smith <danielle.smith@launchbadge.com>
2022-01-11 11:05:05 -0800

    In progress, created index.ts and updated ts.config

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -25,21 +25,3 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 
-
-// Cypress.Commands.add('login', (email, password) => {
-//   email = "logan@launchbadge.com"
-//   password = "Lambda19"
-//   cy.visit('/sign-in')
-//   cy.get('[data-cy=sign-in-modal]').should("be.visible");
-//   cy.get('[data-cy=sign-in-email-input]').should("be.visible");
-//   cy.get('[data-cy=sign-in-email-input]').type(email);
-//   cy.get('[data-cy=sign-in-get-started-button]').should("be.visible");
-//   cy.get('[data-cy=sign-in-get-started-button]').click().then(() => {
-//       cy.get('[data-cy=sign-in-error-message]').should("be.visible");
-//   });
-//   cy.get('[data-cy=sign-in-password-input]').type(password);
-//   cy.get('[data-cy=sign-in-get-started-button]').click().then(() => {
-//       cy.url().should("include", "/wallet");
-//   });
-//   cy.visit("/profile")
-// })
