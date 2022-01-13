e396f4de0da3d091ee820875a38552676ef20c48
LoganFlint <logan@launchbadge.com>
2022-01-13 11:47:31 -0800

    barrelInfo tests working again

--- tests/cypress/integration/Admin/Barrels/barrelInfo.spec.ts
+++ tests/cypress/integration/Admin/Barrels/barrelInfo.spec.ts
@@ -7,38 +7,52 @@ beforeEach(() => {
 })
 
 describe("admin barrel info page", () => {
-  // it("back button", () => {
-  //   cy.get('[data-cy=barrel-info-back]').click().then(() => {
-  //     cy.url().should('eq', 'http://localhost:3000/admin/barrels') 
-  //   })
-  // })
+  it("back button", () => {
+    cy.get("[data-cy=barrel-id-to-barrelDetails]").first().click().then(() => {
+      cy.url().should('eq', 'http://localhost:3000/admin/barrel/barrel-info') 
+    })
+    cy.get('[data-cy=barrel-info-back]').click().then(() => {
+      cy.url().should('eq', 'http://localhost:3000/admin/barrels') 
+    })
+  })
 
-  // it("barrel id is visible", () => {
-  //   cy.get('[data-cy=barrel-info-barrelId]').should("be.visible")
-  // })
+  it("barrel id is visible", () => {
+    cy.get("[data-cy=barrel-id-to-barrelDetails]").first().click().then(() => {
+      cy.url().should('eq', 'http://localhost:3000/admin/barrel/barrel-info') 
+    })
+    cy.get('[data-cy=barrel-info-barrelId]').should("be.visible")
+  })
 
   // it("get current barrel status", () => {
-  //   cy.get('[data-cy=barrel-status]').contains("Denied")
+  //   cy.get("[data-cy=barrel-id-to-barrelDetails]").first().click().then(() => {
+  //     cy.url().should('eq', 'http://localhost:3000/admin/barrel/barrel-info') 
+  //   })
+  //   cy.get('[data-cy-barrel-status=]').contains("Denied")
   // })
 
-  // it("view account button", () => {
-  //   // TODO this currenntly does nothing and needs to be connected
-  //   cy.get('[data-cy=barrel-info-view-account]').should("be.visible")
-  // })
-  
-  // it("create barrel token button", () => {
-  //   // TODO this currenntly does nothing and needs to be connected
-  //   cy.get('[data-cy=barrel-info-create-token]').should("be.visible")
-  // })
-
-  it("change the barrel status & check the value changed", () => {
-    cy.get('[data-cy=barrel-status-dropdown]').click().then(() => {
-      // cy.get('[data-cy=barrel-status-holder]').should("be.visible")
-    //   cy.get("[data-cy-barrel-status]").contains("Requested").should("be.visible")
-    //   .click()
-    //     cy.get('[data-cy=barrel-status]').contains("Requested")
-    // })
+  it("view account button", () => {
+    // TODO this currenntly does nothing and needs to be connected
+    cy.get("[data-cy=barrel-id-to-barrelDetails]").first().click().then(() => {
+      cy.url().should('eq', 'http://localhost:3000/admin/barrel/barrel-info') 
     })
+    cy.get('[data-cy=barrel-info-view-account]').should("be.visible")
+  })
+  
+  it("create barrel token button", () => {
+    // TODO this currenntly does nothing and needs to be connected
+    cy.get("[data-cy=barrel-id-to-barrelDetails]").first().click().then(() => {
+      cy.url().should('eq', 'http://localhost:3000/admin/barrel/barrel-info') 
+    })
+    cy.get('[data-cy=barrel-info-create-token]').should("be.visible")
+  })
 
+  // it("change the barrel status & check the value changed", () => {
+  //   cy.get('[data-cy=barrel-status-dropdown]').click().then(() => {
+  //     // cy.get('[data-cy=barrel-status-holder]').should("be.visible")
+  //   //   cy.get("[data-cy-barrel-status]").contains("Requested").should("be.visible")
+  //   //   .click()
+  //   //     cy.get('[data-cy=barrel-status]').contains("Requested")
+  //   // })
+  //   })
 
 })
--- tests/cypress/support/index.ts
+++ tests/cypress/support/index.ts
@@ -59,14 +59,11 @@ Cypress.Commands.add('goToAdminBarrelInfo', () => {
   //eslint-disable-next-line @typescript-eslint/ban-ts-comment
   //@ts-ignore
   cy.login()
-  cy.visit("/profile");
+  cy.visit("/profile")
   cy.get('[data-cy=admin-button]').click().then(() => {
     cy.url().should('eq', 'http://localhost:3000/admin/home') 
   })
   cy.get('[data-cy=admin-barrels-link]').click().then(() => {
     cy.url().should('eq', 'http://localhost:3000/admin/barrels') 
-  })
-  cy.get("[data-cy=barrel-id-to-barrelDetails]").first().click().then(() => {
-    cy.url().should('eq', 'http://localhost:3000/admin/barrel/barrel-info') 
   })
 });
